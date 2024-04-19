bde466c0c1107d10a2eeddcf3b8aa553f846fbb1
lrq828 <105230694+lrq828@users.noreply.github.com>
2024-04-19 11:44:24 +0800

    Add files via upload

---
+++ scp_ht_plot.R
@@ -0,0 +1,24 @@
+##需先搭建SCP环境，见https://github.com/zhanghao-njmu/SCP
+
+
+library(SCP)
+
+GC <- readRDS("./GC_data/GC_sub_B.rds")
+mir_target <- read.table("./GC_data/target.txt",header = T)
+
+
+GC <- AnnotateFeatures(GC, species = "Homo_sapiens", db = c("TF", "CSPA"))
+
+GC$celltype <- GC@active.ident
+
+ht2 <- FeatureHeatmap(
+  srt = GC, group.by = "celltype", features = mir_target$Gene, feature_split = mir_target$Group,
+  species = "Homo_sapiens", db = c("GO_BP", "GO_MF", "KEGG"), anno_terms = TRUE,
+  feature_annotation = c("TF", "CSPA"), feature_annotation_palcolor = list(c("gold", "steelblue"), c("forestgreen")),
+  height = 5, width = 4,topTerm = 8)
+
+pdf("target_FeatureHeatmap3.pdf", height = 10, width = 23)
+print(ht2$plot)
+dev.off()
+
+
